c2d430278dd46c9a3b8f19a284eb0d6f70781682
Frank <ziang6@ualberta.ca>
2022-06-29 15:44:46 -0600

    congif styled-components

--- tail_website/src/App.tsx
+++ tail_website/src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import tw from "twin.macro"
+import { TopSection } from './containers/TopSection';
+
+
+const AppContainer = tw.div`
+  flex
+  w-full
+  h-full
+`;
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <AppContainer>
+      <TopSection></TopSection>
+    </AppContainer>
   );
 }
 
