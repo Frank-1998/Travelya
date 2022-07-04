597ba8f7f84da6f14724e99321e36a6a7cecc46b
Frank <ziang6@ualberta.ca>
2022-07-03 22:08:29 -0600

    create logo, navBar

---
+++ tail_website/src/components/logo/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const LogoContainer = styled.div`
+
+    ${tw`
+        font-family["ZCOOL Kuaile"]
+        font-black
+        text-2xl
+        text-white
+    `};
+
+`;
+
+export function Logo() {
+    return <LogoContainer>
+            TRAVELYA
+        </LogoContainer>
+};
